44d7b48e9423784964217c151ed6a17f62705cc2
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2020-04-12 18:31:14 +0200

    Improve tests
    Test that the dezoomed image actually has the expected pixels

--- tests/test_local_dezoomifying.rs
+++ tests/test_local_dezoomifying.rs
@@ -1,5 +1,6 @@
 use dezoomify_rs::{Arguments, dezoomify, ZoomError};
 use std::default::Default;
+use image::{self, DynamicImage, GenericImageView};
 
 /// Dezoom a file locally
 #[tokio::test(threaded_scheduler)]
@@ -16,6 +17,17 @@ async fn custom_size_local_zoomify_tiles() -> Result<(), ZoomError> {
     args.outfile = Some(out.clone());
     dezoomify(&args).await?;
 
-    assert!(out.exists());
+    let actual = image::open(&out)?;
+    let expected = image::open("testdata/zoomify/test_custom_size/expected_result.jpg")?;
+    assert_images_equal(actual, expected);
     Ok(())
+}
+
+fn assert_images_equal(a: DynamicImage, b: DynamicImage) {
+    assert_eq!(a.dimensions(), b.dimensions(), "image dimensions should match");
+    for ((_, _, a), (_, _, b)) in a.pixels().zip(b.pixels()) {
+        for (&pa, &pb) in a.0.iter().zip(b.0.iter()) {
+            assert!(pa.max(pb) - pa.min(pb) < 10);
+        }
+    }
 }
