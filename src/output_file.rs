a9af887cb1b9a00ecd992c9eb52335274edd2948
ophir <pere.jobs@gmail.com>
2020-09-13 20:58:32 +0200

    Fix flaky tests
    I'm not very proud of my solution

--- src/output_file.rs
+++ src/output_file.rs
@@ -51,34 +51,39 @@ pub fn get_outname(outfile: &Option<PathBuf>, zoom_name: &Option<String>, size: 
 
 #[cfg(test)]
 mod tests {
+    use std::collections::hash_map::DefaultHasher;
     use std::env::{current_dir, set_current_dir, temp_dir};
     use std::error::Error;
     use std::fs::{create_dir, File, remove_dir_all, remove_file};
-    use std::time::SystemTime;
+    use std::hash::{Hash, Hasher};
+    use std::panic::{catch_unwind, RefUnwindSafe};
+    use std::process::id;
 
     use super::*;
 
-    fn move_to_tmp() -> Result<PathBuf, Box<dyn Error>> {
-        let cwd = temp_dir();
-        set_current_dir(&cwd)?;
-        Ok(cwd)
-    }
-
-    fn in_tmp_dir<T, F: FnMut() -> T>(mut f: F) -> Result<T, Box<dyn Error>> {
-        let cwd = current_dir()?;
-        let mut tmp = temp_dir();
-        tmp.push(format!("dezoomify-rs-{:?}", SystemTime::now()));
-        create_dir(&tmp)?;
-        set_current_dir(&tmp)?;
-        let res = f();
-        set_current_dir(&cwd)?;
-        remove_dir_all(&tmp)?;
-        Ok(res)
+    fn in_tmp_dir<T, F: RefUnwindSafe + Fn(&PathBuf) -> T, H: Hash>(f: F, value: H) -> T {
+        let hasher = &mut DefaultHasher::new();
+        value.hash(hasher);
+        let hash = hasher.finish();
+        let lock = temp_dir().join("dezoomify-rs-lock");
+        while create_dir(&lock).is_err() {} // Wait for the lock to be free
+        let res = catch_unwind(|| {
+            let cwd = current_dir().expect("Unable to getcwd");
+            let tmp = lock.join(format!("dezoomify-rs-test-{}-{:?}", id(), hash));
+            create_dir(&tmp).expect(&format!("Unable to create {:?}", &tmp));
+            set_current_dir(&tmp).expect(&format!("Unable to cd into {:?}", &tmp));
+            let res = f(&tmp);
+            remove_dir_all(&tmp).expect(&format!("Unable to rm -r {:?}", &tmp));
+            set_current_dir(&cwd).expect(&format!("Unable to cd into {:?}", &cwd));
+            res
+        });
+        remove_dir_all(&lock).expect("Unable to remove dezoomify-rs test dir");
+        res.unwrap()
     }
 
     fn assert_filename_ok(filename: &str) -> Result<(), Box<dyn Error>> {
         let outname = get_outname(&None, &Some(filename.to_string()), None);
-        assert_eq!(false, outname.exists(), "get_outname cannot overwrite an existing file");
+        assert_eq!(false, outname.exists(), "get_outname cannot overwrite {:?}", outname);
         File::create(&outname)
             .expect(&format!("Could not to create a file named {:?} for input {:?}", outname, filename));
         remove_file(&outname)?;
@@ -88,7 +93,6 @@ mod tests {
     #[test]
     fn test_special_chars() -> Result<(), Box<dyn Error>> {
         // See https://github.com/lovasoa/dezoomify-rs/issues/29
-        move_to_tmp()?;
         let filenames = vec![
             "? [Question Mark] Australian WWI Poster",
             "The Rocky Mountains, Lander's Peak",
@@ -96,21 +100,20 @@ mod tests {
             "", // test empty name
         ];
         for filename in filenames {
-            in_tmp_dir(|| {
-                assert_filename_ok(filename)
-            })??;
+            in_tmp_dir(|_| {
+                assert_filename_ok(filename).expect(&format!("Invalid filename {}", filename))
+            }, filename);
         }
         Ok(())
     }
 
     #[test]
-    fn test_existing_file() -> Result<(), Box<dyn Error>> {
-        let mut cwd = move_to_tmp()?;
-        let name = "xxx";
-        cwd.push(name);
-        File::create(&cwd)?;
-        assert_filename_ok(name)?;
-        Ok(())
+    fn test_existing_file() {
+        in_tmp_dir(|cwd| {
+            let name = cwd.join("xxx");
+            File::create(&name).expect("cannot create file");
+            assert_filename_ok(&name.to_string_lossy()).expect("Invalid file name")
+        }, "xxx")
     }
 
     #[test]
@@ -125,9 +128,9 @@ mod tests {
             (Some("test.tiff".into()), Some("hello".to_string()), Some(Vec2d { x: 1000, y: 1000 }), "test.tiff"),
         ];
         for (outfile, zoom_name, size, expected_result) in tests.into_iter() {
-            in_tmp_dir(|| {
+            in_tmp_dir(|_| {
                 assert_eq!(get_outname(&outfile, &zoom_name, size), PathBuf::from(expected_result))
-            }).unwrap();
+            }, (&outfile, &zoom_name, &size));
         }
     }
 }
