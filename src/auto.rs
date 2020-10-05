278f9f5d45f09acf2a8d2452c101fe221cb9b7d7
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2020-10-05 19:27:06 +0200

    Change the behavior of the "auto" dezoomer
    Previously, it returned immediately when it found at least one zoom level.
    Now, it waits until all dezoomers have proposed all the zoom levels they could find to aggregate them.
    This also gives better error messages when all dezoomers fail.
    
    See: https://github.com/lovasoa/dezoomify-rs/issues/80

--- src/auto.rs
+++ src/auto.rs
@@ -1,5 +1,6 @@
-use crate::dezoomer::{Dezoomer, DezoomerError, DezoomerInput, ZoomLevels};
+use crate::dezoomer::{Dezoomer, DezoomerError, DezoomerInput, ZoomLevels, ZoomLevel};
 use log::{info, debug};
+use crate::errors::DezoomerError::NeedsData;
 
 pub fn all_dezoomers(include_generic: bool) -> Vec<Box<dyn Dezoomer>> {
     let mut dezoomers: Vec<Box<dyn Dezoomer>> = vec![
@@ -21,12 +22,18 @@ pub fn all_dezoomers(include_generic: bool) -> Vec<Box<dyn Dezoomer>> {
 
 pub struct AutoDezoomer {
     dezoomers: Vec<Box<dyn Dezoomer>>,
+    errors: Vec<(&'static str, DezoomerError)>,
+    successes: Vec<ZoomLevel>,
+    needs_uris: Vec<String>,
 }
 
 impl Default for AutoDezoomer {
     fn default() -> Self {
         AutoDezoomer {
             dezoomers: all_dezoomers(false),
+            errors: vec![],
+            successes: vec![],
+            needs_uris: vec![],
         }
     }
 }
@@ -37,26 +44,24 @@ impl Dezoomer for AutoDezoomer {
     }
 
     fn zoom_levels(&mut self, data: &DezoomerInput) -> Result<ZoomLevels, DezoomerError> {
-        let mut errs = vec![];
-        let mut successes = Vec::new();
-        let mut needs_uri = None;
         // TO DO: Use drain_filter when it is stabilized
         let mut i = 0;
         while i != self.dezoomers.len() {
             let dezoomer = &mut self.dezoomers[i];
             let keep = match dezoomer.zoom_levels(data) {
                 Ok(mut levels) => {
-                    successes.append(&mut levels);
+                    info!("dezoomer '{}' found {} zoom levels", dezoomer.name(), levels.len());
+                    self.successes.append(&mut levels);
                     true
                 }
-                Err(e @ DezoomerError::NeedsData { .. }) => {
-                    debug!("{} requested more data: {}", dezoomer.name(), e);
-                    needs_uri = Some(e);
+                Err(DezoomerError::NeedsData { uri }) => {
+                    info!("dezoomer '{}' requested to load {}", dezoomer.name(), &uri);
+                    self.needs_uris.push(uri);
                     true
                 }
                 Err(e) => {
                     debug!("{} cannot process this image: {}", dezoomer.name(), e);
-                    errs.push((dezoomer.name(), e));
+                    self.errors.push((dezoomer.name(), e));
                     false
                 }
             };
@@ -66,10 +71,14 @@ impl Dezoomer for AutoDezoomer {
                 self.dezoomers.remove(i);
             }
         }
-        if successes.is_empty() {
+        if let Some(uri) = self.needs_uris.pop() {
+            Err(NeedsData { uri })
+        } else if self.successes.is_empty() {
             info!("No dezoomer can dezoom {:?}", data.uri);
-            Err(needs_uri.unwrap_or_else(|| DezoomerError::wrap(AutoDezoomerError(errs))))
+            let errs = std::mem::take(&mut self.errors);
+            Err(DezoomerError::wrap(AutoDezoomerError(errs)))
         } else {
+            let successes = std::mem::take(&mut self.successes);
             Ok(successes)
         }
     }
