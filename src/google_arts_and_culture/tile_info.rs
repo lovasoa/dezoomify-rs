d76b6313445a25936e2df633942bb717ae50a2c0
lovasoa <pere.jobs@gmail.com>
2023-08-07 09:29:48 +0200

    fix tests

--- src/google_arts_and_culture/tile_info.rs
+++ src/google_arts_and_culture/tile_info.rs
@@ -95,8 +95,7 @@ mod tests {
 
     #[test]
     fn test_xml_parse() {
-        let infos: TileInfo = serde_xml_rs::from_str(r#"
-            <?xml version="1.0" encoding="UTF-8"?>
+        let infos: TileInfo = serde_xml_rs::from_str(r#"<?xml version="1.0" encoding="UTF-8"?>
             <TileInfo tile_width="512" tile_height="512" full_pyramid_depth="5" origin="TOP_LEFT" timestamp="1564671682" tiler_version_number="2" image_width="5436" image_height="4080">
                 <pyramid_level num_tiles_x="1" num_tiles_y="1" inverse_scale="16" empty_pels_x="173" empty_pels_y="257"/>
                 <pyramid_level num_tiles_x="2" num_tiles_y="1" inverse_scale="8" empty_pels_x="345" empty_pels_y="2"/>
